b63110102008d160a441d765c26be1d8c5fe96b1
Clint Armstrong <clint@clintarmstrong.net>
2016-01-08 21:49:59 -0500

    No idea what I'm doing

--- vxlan/driver.go
+++ vxlan/driver.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	log "github.com/Sirupsen/logrus"
-	dknet "github.com/docker/go-plugins-helpers/network"
+	"github.com/docker/go-plugins-helpers/network"
 	"github.com/samalba/dockerclient"
 	"github.com/socketplane/libovsdb"
 	"github.com/vishvananda/netlink"
@@ -25,26 +25,67 @@ const (
 )
 
 type Driver struct {
-	dknet.Driver
+	network.Driver
 	networks map[string]*NetworkState
 }
 
 // NetworkState is filled in at network creation time
 // it contains state that we wish to keep for each network
 type NetworkState struct {
-	BridgeName string
-	VXLanName  string
+	BridgeName	string
+	VXLanName	string
+	VXLanID		int
 }
 
-func (d *Driver) CreateNetwork(r *dknet.CreateNetworkRequest) error {
+func NewDriver() (*Driver, error) {
+	d := &Driver{
+		networks: make(map[string]*NetworkState),
+	}
+	return d, nil
+}
+
+// NewHandler initializes the request handler with a driver implementation.
+func NewHandler(driver Driver) *Handler {
+	h := &Handler{driver, sdk.NewHandler(manifest)}
+	h.initMux()
+	return h
+}
+
+func (d *Driver) CreateNetwork(r *network.CreateNetworkRequest) error {
 	log.Debugf("Create network request: %+v", r)
+
+	name := r.NetworkID
 
 	//if r.Options == nil {
 	//	return "", fmt.Errorf("No options provided")
 	//}
 
-	//vxlanID := "42"
+	vxlanName := "vxlan42"
+	vxlanID := 42
 	//if r.Options["vxlanID"] != nil {
 	//	vxlanID = r.Options["vxlanID"]
 	//}
+
+	BridgeName := "br_vxlan42"
+
+
+	bridge := &netlink.Bridge{
+		LinkAttrs: netlink.LinkAttrs{Name: ns['BridgeName'] },
+	}
+	netlink.LinkAdd(bridge)
+
+	vxlan := &netlink.Vxlan{
+		LinkAttrs: netlink.LinkAttrs{Name: ns['VXlanName']},
+		VxlanId:   ns['VXLanID'],
+	}
+	netlink.LinkAdd(vxlan)
+
+	ns := &NetworkState {
+		VXLan:		vxlan,
+		Bridge:		bridge,
+	}
+	d.networks[r.NetworkID] = ns
+
+	netlink.LinkSetMaster(vxlan, bridge)
+
 }
