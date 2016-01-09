363d44f17e0f9a4e7d87dfbd3ed07097ef4f9dd3
Clint Armstrong <clint@clintarmstrong.net>
2016-01-08 20:07:39 -0500

    main.go

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/clinta/docker-vxlan-plugin/vxlan"
+	"github.com/codegangsta/cli"
+	dknet "github.com/docker/go-plugins-helpers/network"
+)
+
+const (
+	version = "0.1"
+)
+
+func main() {
+
+	var flagDebug = cli.BoolFlag{
+		Name:  "debug, d",
+		Usage: "enable debugging",
+	}
+	app := cli.NewApp()
+	app.Name = "don"
+	app.Usage = "Docker vxLan Networking"
+	app.Version = version
+	app.Flags = []cli.Flag{
+		flagDebug,
+	}
+	app.Action = Run
+	app.Run(os.Args)
+}
+
+// Run initializes the driver
+func Run(ctx *cli.Context) {
+	if ctx.Bool("debug") {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	d, err := vxlan.NewDriver()
+	if err != nil {
+		panic(err)
+	}
+	h := dknet.NewHandler(d)
+	h.ServeUnix("root", "vxlan")
+}
