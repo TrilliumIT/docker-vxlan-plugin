710ca45704ee9bdf0e10f49ddf51e55bfe3e3697
Clint Armstrong <clint@clintarmstrong.net>
2016-01-13 21:09:12 -0500

    improve make script

--- make.sh
+++ make.sh
@@ -1,3 +1,2 @@
-docker run . cat /go/bin/docker-vxlan-plugin >> docker-vxlan-plugin
-docker build -t docker-vxlan-plugin . && docker run docker-vxlan-plugin cat /go/bin/docker-vxlan-plugin > docker-vxlan-plugin
+docker build -f ./Dockerbuild -t docker-vxlan-plugin-build . && docker run docker-vxlan-plugin-build cat /go/bin/docker-vxlan-plugin > docker-vxlan-plugin
 chmod +x docker-vxlan-plugin
