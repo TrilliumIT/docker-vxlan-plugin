a14b931fac5ef1d03af00aa1d107a5cdf2e26b79
Clint Armstrong <clint@clintarmstrong.net>
2016-05-05 08:36:56 -0400

    build script working

--- make.sh
+++ make.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 set -e
 
-LATEST_RELEASE=$(git describe --tags --apprev=0 | sed "s/^v//g")
+LATEST_RELEASE=$(git describe --tags --abbrev=0 | sed "s/^v//g")
 DOCKER_VER=$(grep "ENV VER=" Dockerfile | sed "s/^ENV VER=v//g")
 MAIN_VER=$(grep "version = " main.go | sed 's/[ \t]*version[ \t]*=[ \t]*//g' | sed 's/"//g')
-VERS=$(echo "${LATEST_RELEASE}\n${DOCKER_VER}\n${MAIN_VER}")
+VERS="${LATEST_RELEASE}\n${DOCKER_VER}\n${MAIN_VER}"
 
 # For tagged commits
 if [ $(git describe --tags) = ${LATEST_RELEASE} ] ; then
-	if [ $(echo ${VERS} | uniq | wc -l) -gt 1 ]
+	if [ $(printf ${VERS} | uniq | wc -l) -gt 1 ] ; then
 		echo "This is a release and the versions don't match"
+		exit 1
+	fi
+else
+	if [ $(printf ${VERS} | uniq | wc -l) = 1 ] ; then
+		echo "Please increment the version in main.go"
+		exit 1
+	fi
+	if [ $(printf ${VERS} | sort -V | tail -n l) != ${MAIN_VER} ] ; then
+		echo "Please increment the version in main.go"
 		exit 1
 	fi
 fi
 
-if [ $(echo ${VERS} | sort -V | head -n l) != ${LATEST_RELEASE} ] ; then
-	echo "Current versions are less than the latest release"
-	exit 1
-fi
 
-docker build -f ./Dockerbuild -t docker-vxlan-plugin-build . && docker run docker-vxlan-plugin-build cat /go/bin/docker-vxlan-plugin > docker-vxlan-plugin
+docker build -f ./Dockerbuild -t docker-vxlan-plugin-build:v${MAIN_VER} . && docker run docker-vxlan-plugin-build:v${MAIN_VER} cat /go/bin/docker-vxlan-plugin > docker-vxlan-plugin
 chmod +x docker-vxlan-plugin
+docker build -f ./Dockerlocal -t docker-vxlan-plugin:v${MAIN_VER} .
