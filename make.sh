405e43523d5ce27cca39238944682ab44e2dbec1
Clint Armstrong <clint@clintarmstrong.net>
2016-05-31 16:43:45 -0400

    fix build script

--- make.sh
+++ make.sh
@@ -2,15 +2,8 @@
 set -e
 
 LATEST_RELEASE=$(git describe --tags --abbrev=0 | sed "s/^v//g")
-DOCKER_VER=$(grep "ENV VER=" Dockerfile | sed "s/^ENV VER=v//g")
 MAIN_VER=$(grep "version = " main.go | sed 's/[ \t]*version[ \t]*=[ \t]*//g' | sed 's/"//g')
 VERS="${LATEST_RELEASE}\n${DOCKER_VER}\n${MAIN_VER}"
-
-# Dockerfile should always match latest release
-if [ "${DOCKER_VER}" != "${LATEST_RELEASE}" ] ; then
-	echo "Dockerfile does not match latest tag"
-	exit 1
-fi
 
 # For tagged commits
 if [ "$(git describe --tags)" = "${LATEST_RELEASE}" ] ; then
